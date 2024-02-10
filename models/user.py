8249e7afc1d2e4fba87d51cb1f3bd6a2eb458835
ABgitme <abebekezhabesha@gmail.com>
2024-02-11 00:00:09 +0300

    task 8 update attribules

--- models/user.py
+++ models/user.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
+"""
+Module models/user.py: Defines the User class,
+representing users in the system.
+
+This module implements the User class, which inherits
+from the BaseModel class to provide
+basic functionalities for data persistence
+and management. The User class stores information
+such as email, password, first name, and last name.
+
+"""
 from models.base_model import BaseModel
 
 class User(BaseModel):
-    """User class inherits from BaseModel."""
-    def __init__(self, *args, **kwargs):
-        if len(kwargs) == 0:
-            # No keyword arguments provided, handle this case specifically
-            super().__init__()  # Call BaseModel's __init__ without arguments
-            self.email = ''
-            self.password = ''
-            self.first_name = ''
-            self.last_name = ''
+    """
+    The User class represents a user within the system.
 
-        else:
-            # Keyword arguments are present, proceed as in snippet 2
-            super().__init__(*args, **kwargs)
-            self.email = kwargs.get('email', '')
-            self.password = kwargs.get('password', '')
-            self.first_name = kwargs.get('first_name', '')
-            self.last_name = kwargs.get('last_name', '')
+    This class inherits from the BaseModel class,
+    providing functionality for data
+    persistence and management. It defines the
+    following attributes:
+
+    - email: string - The user's email address (default: "").
+    - password: string - The user's password (default: "").
+    - first_name: string - The user's first name (default: "").
+    - last_name: string - The user's last name (default: "").
+
+    You can create User instances with or without
+    providing values for these attributes.
+
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
