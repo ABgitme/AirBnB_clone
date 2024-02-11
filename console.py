8c01f64d8ddff29e1583c1517a9e96bc2351ed76
ABgitme <abebekezhabesha@gmail.com>
2024-02-11 17:16:31 +0300

    task 15  add

--- console.py
+++ console.py
@@ -24,6 +24,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 
+
 class HBNBCommand(cmd.Cmd):
     """HBNB Command Line Interface
 
@@ -44,8 +45,8 @@ class HBNBCommand(cmd.Cmd):
         prompt (str): Prompt string for the command line interface.
     """
     valid_classes = {'BaseModel': BaseModel, 'User': User, 'Amenity': Amenity,
-                        'City': City, 'State': State, 'Place': Place,
-                        'Review': Review}
+                    'City': City, 'State': State, 'Place': Place,
+                    'Review': Review}
     ERROR_ATT_VALUE = "** value missing **"
     ERROR_NO_ID_FOUND = "** no instance found **"
     ERROR_CLASS_NOT_EXIST = "** class doesn't exist **"
@@ -215,7 +216,8 @@ class HBNBCommand(cmd.Cmd):
         if method_name[0] in ['show', 'destroy', 'update']:
             if len(method_name) > 1:
                 method_args = method_name[1].strip(')').split(',')
-                method_args = [argval.strip().strip('"') for argval in method_args]
+                method_args = [argval.strip().strip('"')
+                                for argval in method_args]
         method_dict = {
             'update': self.do_update,
             'destroy': self.do_destroy,
